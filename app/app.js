e2c13f55fcd9f520d63deeb601c31b1f1e758b33
justyna124 <justyna124@interia.pl>
2017-02-20 12:58:20 +0100

    clean project

--- app/app.js
+++ app/app.js
@@ -1,14 +1,6 @@
 (function ()
 {
     'use strict';
-    var app = angular.module('training', []);
 
-    app.controller('trainController', function ()
-    {
-        this.info = 'Ucze sie angulara';
-        this.cup = {color: 'red'};
-        this.colours = ['white', 'green', 'yellow', 'pink', 'blue'];
-        this.users = [{name: 'Justyna', email: 'justyna@justyna.com'}, {name: 'Ania', email: 'ania@ania.com'}];
-    });
 
 })();
